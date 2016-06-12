811438b4d8111f1e951a4f6d52117964a0ea7169
Daniela <Daniela@DESKTOP-2FBT2UR>
2016-06-12 10:52:25 +0200

    aggiunta regex telefoni

--- ThirdProject/src/main/java/it/uniroma3/agiw/ThirdProject/RegexExtraction.java
+++ ThirdProject/src/main/java/it/uniroma3/agiw/ThirdProject/RegexExtraction.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 public class RegexExtraction {
 	
 	private String email_regex = "[^\\s@<>]+@[^\\s<>]+\\.[^\\s@<>]+";
+	private String telephone_regex = "[+(]?([0-9]{2,})[-.) ]?([ .-]?)([0-9]{2,})\\2([0-9]{3,})";
 
 	public RegexExtraction(){}
 	
@@ -15,6 +16,20 @@ public class RegexExtraction {
 		List<String> output = new ArrayList<String>();
 
 		Pattern pattern = Pattern.compile(email_regex, Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
+		Matcher m = pattern.matcher(html);
+		
+		while(m.find()){
+			output.add(m.group());
+		}
+		
+		return output;
+		
+	}
+	
+	public List<String> doTelephoneExtraction(String html){
+		List<String> output = new ArrayList<String>();
+
+		Pattern pattern = Pattern.compile(telephone_regex, Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
 		Matcher m = pattern.matcher(html);
 		
 		while(m.find()){
